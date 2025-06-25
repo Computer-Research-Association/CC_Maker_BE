d1f916305add8a64632119e7f7625ba65f1efa09
losecow <100589396+Losecow@users.noreply.github.com>
2025-06-25 11:41:46 +0900

    스프링부트 기본 파일 업로드

---
+++ ccgo/ccgo/src/main/java/com/ccapp/ccgo/CCgoApplication.java
@@ -0,0 +1,13 @@
+package com.ccapp.ccgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CCgoApplication.class, args);
+	}
+
+}
---
+++ ccgo/ccgo/src/test/java/com/ccapp/ccgo/CCgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccapp.ccgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CCgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
