8d43f97d779e8a7ee74ab3688b384159c66e00d5
bendy41 <sleepdeath315@gmail.com>
2025-06-27 09:28:37 +0900

    Add exception

--- src/main/java/com/ccapp/ccgo/exception/CustomException.java
+++ src/main/java/com/ccapp/ccgo/exception/CustomException.java
@@ -1,4 +1,19 @@
 package com.ccapp.ccgo.exception;
 
-public class CustomException {
+import org.springframework.http.HttpStatus;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    //HTTP 상태
+    private final HttpStatus status;
+
+    //에러메세지, HTTP 상태 코드 (status)
+    public CustomException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+
 }
--- src/main/java/com/ccapp/ccgo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ccapp/ccgo/exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.ccapp.ccgo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+//다양한 에러 처리 클래스
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, String>> handleCustomException(CustomException ex) {
+        return ResponseEntity
+                .status(ex.getStatus()) // 예외에서 상태코드 가져오기
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        return ResponseEntity
+                .status(500)
+                .body(Map.of("message", "서버 오류가 발생했습니다."));
+    }
 }
