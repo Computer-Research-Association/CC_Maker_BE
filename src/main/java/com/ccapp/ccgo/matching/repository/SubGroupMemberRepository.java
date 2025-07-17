9804b96e26ad1a5bb3010a0f284b534445e03dc1
bendy41 <sleepdeath315@gmail.com>
2025-07-17 15:29:07 +0900

    Final Done

--- src/main/java/com/ccapp/ccgo/matching/repository/SubGroupMemberRepository.java
+++ src/main/java/com/ccapp/ccgo/matching/repository/SubGroupMemberRepository.java
@@ -20,13 +20,12 @@ public interface SubGroupMemberRepository extends JpaRepository<SubGroupMember, 
 
     //subgroup 자신을 제외한 팀원 이름조회
     @Query("SELECT m FROM SubGroupMember m WHERE m.subGroup = " +
-            "(SELECT sm.subGroup FROM SubGroupMember sm WHERE sm.user.id = :userId)")
-    List<SubGroupMember> findBySameSubGroup(@Param("userId") Long userId);
+            "(SELECT sm.subGroup FROM SubGroupMember sm WHERE sm.user.id = :userId AND sm.subGroup.team.teamId = :teamId)")
+    List<SubGroupMember> findBySameSubGroup(@Param("userId") Long userId, @Param("teamId") Long teamId);
 
 
     @Query("SELECT s.user FROM SubGroupMember s WHERE s.subGroup.id = :subGroupId")
     List<User> findUsersBySubGroupId(@Param("subGroupId") Long subGroupId);
-
 
 
     @Query("SELECT sgm.subGroup.id FROM SubGroupMember sgm WHERE sgm.subGroup.team.teamId = :teamId AND sgm.user.id = :userId")
